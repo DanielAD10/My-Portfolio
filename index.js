ea4a7fc919a38943f047dce39677d76ea0476dfe
RileyManda <rileymanda69@gmail.com>
2023-05-22 13:55:42 +0200

    Linter error fixes and removed unnamed functions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function openMobileMenu() {
-  var mobileMenu = document.getElementById('mobile-menu');
+const openMobileMenu = function openMobileMenuHandler() {
+  const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.style.display = 'flex'; // Show the mobile menu
-}
+};
 
-function closeMobileMenu() {
-  var mobileMenu = document.getElementById('mobile-menu');
+const closeMobileMenu = function closeMobileMenuHandler() {
+  const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.style.display = 'none'; // Hide the mobile menu
-}
+};
+
+document.getElementById('nav-btn').addEventListener('click', openMobileMenu);
+document.getElementById('close-btn').addEventListener('click', closeMobileMenu);
