366dcaae6489d1018e1135a3a5791f43dad48c07
RileyManda <rileymanda69@gmail.com>
2023-05-22 12:57:50 +0200

    Added mobile menu click events  and mobile menu styling

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function showAlert() {
-  alert('Clicked!');
+function openMobileMenu() {
+  var mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.remove('hidden');
+}
+
+function closeMobileMenu() {
+  var mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.add('hidden');
 }
