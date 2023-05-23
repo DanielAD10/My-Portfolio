c9bbec9a37c19f8fd7260e17ff538f43e40089fa
RileyManda <rileymanda69@gmail.com>
2023-05-23 13:44:04 +0200

    Added works card object

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const workcards = [
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: '',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+];
+export default workcards;
+
 const openMobileMenu = function openMobileMenuHandler() {
   const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.style.display = 'flex'; // Show the mobile menu
