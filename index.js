1e39a63b440322c8cc2e709d0444d38da1575349
RileyManda <rileymanda69@gmail.com>
2023-05-23 16:14:35 +0200

    Bug fix in details container

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import workcards from './works-data.js';
-import aboutcards from './about-data.js';
-
-// itirate through the worksdata
-// for each card in workscards  loop and access the objects values
-// itirate through the aboutdata
-// for each card in aboutcards loop and access the objects values
 const openMobileMenu = function openMobileMenuHandler() {
   const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.style.display = 'flex'; // Show the mobile menu
@@ -15,11 +8,11 @@ const closeMobileMenu = function closeMobileMenuHandler() {
   mobileMenu.style.display = 'none'; // Hide the mobile menu
 };
 
+const openDetailView = function openDetailHandler() {
+  const detailView = document.getElementById('detail-container');
+  detailView.style.display = 'flex'; // Show the mobile detail view
+};
+
 document.getElementById('nav-btn').addEventListener('click', openMobileMenu);
 document.getElementById('close-btn').addEventListener('click', closeMobileMenu);
-
-// open moobile details
-const openDetails = function openDetailHandler() {
-  document.querySelector('.detail-container').style.display = 'block';
-};
-document.getElementById('project-btn').addEventListener('click', openDetails);
+document.getElementById('card-btn').addEventListener('click', openDetailView);
--- works-data.js
+++ works-data.js
@@ -42,4 +42,30 @@ const workscards = [
     projectLink: '',
   },
 ];
-export default workscards;
+// export default workscards;
+
+const cardContainer = document.querySelector('#card-workflow');
+
+workscards.forEach((card) => {
+  const cardElement = document.createElement('div');
+  cardElement.classList.add('card');
+
+  const titleElement = document.createElement('h2');
+  titleElement.textContent = `${card.title} ${card.subtitle}`;
+  cardElement.appendChild(titleElement);
+
+  const tagsList = document.createElement('ul');
+  card.tags.forEach((tag) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = tag;
+    tagsList.appendChild(listItem);
+  });
+  cardElement.appendChild(tagsList);
+
+  const projectButton = document.createElement('button');
+  projectButton.classList.add('btn', 'card-btn');
+  projectButton.textContent = 'See Project';
+  cardElement.appendChild(projectButton);
+
+  cardContainer.appendChild(cardElement);
+});
