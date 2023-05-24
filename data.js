1ff98b46d0100f284ca77189fab780687e3cdb49
RileyManda <rileymanda69@gmail.com>
2023-05-24 10:41:01 +0200

    Works cards dynamic js and data

---
+++ data.js
@@ -0,0 +1,62 @@
+export const workcards = [
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    detailview: '',
+  },
+];
+
+export const aboutcards = [
+  {
+    icon: './assets/icons/languages.svg',
+    title: 'Language',
+    tags: ['JavaScript', 'Ruby', 'HTML', 'CSS'],
+  },
+  {
+    icon: './assets/icons/frameworks.svg',
+    title: 'Frameworks',
+    tags: ['React.js', 'Ruby on Rails', 'RSpec', 'Capybara', 'Selenium'],
+  },
+  {
+    icon: './assets/icons/skills.svg',
+    title: 'Skills',
+    tags: [
+      'Database Management',
+      'Version Control',
+      'CLI',
+      'Web Development',
+      'API Design',
+    ],
+  },
+];
