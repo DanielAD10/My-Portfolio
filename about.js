25346c51ae16f1f8fafe46b06444920f64001e62
RileyManda <rileymanda69@gmail.com>
2023-05-24 10:52:03 +0200

    Added about js script to html file

---
+++ about.js
@@ -0,0 +1,33 @@
+import { aboutcards } from './data.js';
+
+const generateAboutCardHTML = (aboutcard) => {
+  const { icon, title, tags } = aboutcard;
+  const tagsHTML = tags
+    .map((tag) => `<li class="list-items common-tags-items">${tag}</li>`)
+    .join('');
+  const cardHTML = `
+    <div class="card">
+      <div class="logo">
+        <img src="${icon}" alt="${title} logo">
+      </div>
+      <div class="box">
+        <h2 class="text common-text">${title}</h2>
+        <ul class="common-tags list-box common-box">
+          ${tagsHTML}
+        </ul>
+      </div>
+    </div>
+  `;
+
+  return cardHTML;
+};
+
+const displayAboutCards = () => {
+  const cardWorkflow = document.querySelector('.card-workflow');
+  aboutcards.forEach((aboutcard) => {
+    const cardHTML = generateAboutCardHTML(aboutcard);
+    cardWorkflow.insertAdjacentHTML('beforeend', cardHTML);
+  });
+};
+
+displayAboutCards();
